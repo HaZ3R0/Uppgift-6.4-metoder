f629b0ca8f136553a8749f7f259ac128440f120f
HaZ3R0 <megamackan@outlook.com>
2024-02-14 11:02:52 +0100

    Add project files.

---
+++ Uppgift 6.4 metoder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HejAmeen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in din bas");
+            double bas = double.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in din exponent");
+            double exponent = double.Parse(Console.ReadLine());
+            Console.WriteLine("talet blir " + Potens (bas, exponent));
+        }
+
+        static double Potens(double bas, double exponent)
+        {
+            double a = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                a *= bas;
+            }
+            return a;
+        }
+    }
+}
